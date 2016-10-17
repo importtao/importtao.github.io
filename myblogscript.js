ad0c0c124e97dc5b407b97fffaa73a1c623c6169
ZT <import_tao@163.com>
2016-10-17 17:11:38 +0800

    Add files via upload

---
+++ myblogscript.js
@@ -0,0 +1,83 @@
+
+function f12(argument) {
+			//confirm("头皮的风格！");
+			// body...
+			var m = document.getElementById("id3");
+			
+			m.style.top="180px";
+			
+		}
+		function f11(argument) {
+			var m;
+			m = document.getElementById("id3");
+			
+			m.style.top="200px";
+			
+		}
+		function f22(argument) {
+			//confirm("头皮的风格！");
+			// body...
+			var m;
+			m = document.getElementById("id4");
+			m.style.top="180px";
+		}
+		function f21(argument) {
+			//confirm("头皮的风格！");
+			// body...
+			var m;
+			m = document.getElementById("id4");
+			m.style.top="200px";
+		}
+		function f32(argument) {
+			//confirm("头皮的风格！");
+			// body...
+			var m;
+			m = document.getElementById("id5");
+			m.style.top="180px";
+		}
+		function f31(argument) {
+			//confirm("头皮的风格！");
+			// body...
+			var m;
+			m = document.getElementById("id5");
+			m.style.top="200px";
+		}
+		// var t=function (id) {
+		// 	return document.getElementById(id);
+		// }
+		// t("closeb").onclick= function(){
+		// 	//t("closeb").idname='test'
+		// 	t("closeb").style.display='none';
+		// }
+		function close1(argument){
+			//t("closeb").idname='test'
+			var m1 = document.getElementById("ad");
+			m1.style.display ='none';
+			//var m1 = document.getElementById("ad");
+			//m1.className="test";
+		}
+		//confirm("asasd");confirm("asasd");
+		function wo(argument) {
+			window.open('adT.html');
+		}function close2(argument){
+			//t("closeb").idname='test'
+			var m1 = document.getElementById("adud");
+			var m2 = document.getElementById("open2");
+			var m3 = document.getElementById("close2");
+			m1.style.display ='none';
+			m2.style.display ='block';
+			m3.style.display ='none';
+			//var m1 = document.getElementById("ad");
+			//m1.className="test";
+		}function open2(argument){
+			//t("closeb").idname='test'
+			var m1 = document.getElementById("adud");
+			var m2 = document.getElementById("close2");
+			var m3 = document.getElementById("open2");
+			m1.style.height="400px";
+			m1.style.display='block';
+			m2.style.display='block';
+			m3.style.display ='none';
+			//var m1 = document.getElementById("ad");
+			//m1.className="test";
+		}
